4b70bbc53caccd9eda47d99a4092b9ebaeda1cae
ChristopherDuggan <christopherduggan@gmail.com>
2024-05-31 11:27:25 -0400

    Add new fruit view

--- server.js
+++ server.js
@@ -12,8 +12,27 @@ mongoose.connection.on('connected', () => {
 
 const Fruit = require('./models/fruit.js');
 
+app.use(express.urlencoded({ extended: false} ));
+
 app.get('/', async (req, res) => {
   res.render('index.ejs');
+});
+
+app.get('/fruits/new', (req, res) => {
+  res.render('fruits/new.ejs');
+});
+
+app.post('/fruits', async (req, res) => {
+  req.body.isReadyToEat
+  if ( req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat  = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  await Fruit.create(req.body);
+
+  res.redirect('/fruits/new');
 });
 
 app.listen(3000, () => {
