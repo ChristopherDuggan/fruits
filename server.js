f0fbf920377b2999562136e331b23a5a63118b87
ChristopherDuggan <christopherduggan@gmail.com>
2024-09-26 12:15:32 -0400

    add .env, connect to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+// the other way to bring in dotenv:
+// require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
