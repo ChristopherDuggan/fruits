461e2606c3bf9699325403b56216327e94dc6e47
ChristopherDuggan <christopherduggan@gmail.com>
2024-09-27 10:48:37 -0400

    add edit and delete functionality

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ dotenv.config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 const Fruit = require("./models/fruit.js");
 
@@ -49,6 +53,30 @@ app.get("/fruits/:fruitId", async (req, res) => {
   // const foundFruit = await Fruit.find({ _id: req.params.fruitId });
   res.render("fruits/show.ejs", { fruit: foundFruit });
 });
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  if(req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  res.redirect(`/fruits/${req.params.fruitId}`);
+}); 
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
